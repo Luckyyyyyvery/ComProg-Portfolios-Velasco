65caa67fea949e83c310cb80bef3524cfcb18084
Luckyyyyyvery <velascojosemario@gmail.com>
2026-05-12 23:13:02 +0800

    Add files via upload

---
+++ ComProg portfolio 1 - Velasco.py
@@ -0,0 +1,23 @@
+# ---------------------------------------------------------
+# PROGRAM: LESSON ON PHISHING
+# ---------------------------------------------------------
+
+print('!!! ERROR: GCASH SECURITY ALERT !!!')
+print('GCash suspects a transaction was made by a foreign device.')
+print('Log in below to verify your identity and secure your account.')
+print('-------------------------------------------')
+
+number_input = input('Enter Registered mobile no.: ')
+mpin_input = input('Enter MPIN/Password: ')
+
+print('\n!!! LESSON ON PHISHING !!!')
+print('HAHAHA! You just got phished!')
+print('I have captured your email: ' + number_input)
+print('I have captured your pin number: ' + mpin_input)
+
+stolen_funds = 999.0 
+print('Total amount "stolen" from your account: P' + str(stolen_funds))
+
+print('\n-------------------------------------------')
+print('LESSON: Never trust a random pop up with your data.')
+print('Always use the official app to check your account status!')
